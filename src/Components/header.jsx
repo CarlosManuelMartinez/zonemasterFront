ccf6cc9e0fa6b6b0895d09a3ce0f2b25450d1e3f
Carlos Manuel Martinez Pomares <75163221+CarlosManuelMartinez@users.noreply.github.com>
2025-01-12 21:30:39 +0100

    segundo comit

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -4,7 +4,7 @@ import Button from "@mui/material/Button";
 export function HeaderZonemaster() {
   return (
     <header>
-      <div id="id_div_header">ZONEMASTER PRO v.1.0.3</div>
+      <div id="id_div_header">ZoneMaster PRO v.1.0.3</div>
     </header>
   );
 }
